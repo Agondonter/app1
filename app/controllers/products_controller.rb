0ae1ecc04ddd03710b2b08340424b7920a771034
Agondonter <alexdikappa@gmail.com>
2018-11-03 17:19:47 +0100

    5.2 & 5.3 Corrected search functionality & typo in test folder for mailers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,12 +4,6 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    if Rails.env.development?
-      Product.where('name LIKE ?', '%#{search_term}%')
-    else
-      Product.where('name ilike ?', '%#{search_term}%')
-    end
-
     if params[:q]
       search_term = params[:q]
       @products = Product.search(search_term)
