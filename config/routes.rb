91ec7d6e08c0464a1361b9d5c0f5878e760fdf89
Agondonter <alexdikappa@gmail.com>
2018-11-21 22:22:17 +0100

    6.6 Minor tweaks to try and resolve the app crash on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,14 @@ Rails.application.routes.draw do
   resources :products do
     resources :comments
   end
+
+  post 'payments/create'
+
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
   post 'simple_pages/thank_you'
-  post 'payments/create'
   root 'simple_pages#landing_page'
 
   resources :orders, only: [:index, :show, :create, :destroy]
