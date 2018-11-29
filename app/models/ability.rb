2cd05068f4a2ab82a83ec3f774fbc2b44a123314
Agondonter <alexdikappa@gmail.com>
2018-11-29 11:49:43 +0100

    6.11 Removed caching from products show page

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,13 @@
 class Ability
   include CanCan::Ability
   
-  def initialize(user)
-    if 
-      user ||= User.new # guest user (not logged in)
-      can :manage, User, id: user.id
-    else 
-      user.present? && user.admin? # administrator user (logged in)
+  def initialize(user) 
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
       can :manage, :all
+    else
+      can :manage, User, id: user.id
+      can :manage, Comment, id: user.id
     end
   end
 end
