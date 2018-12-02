0fd9ad6fa7e24b0a35f47c0c2883b53826df30f3
Agondonter <alexdikappa@gmail.com>
2018-12-02 23:24:15 +0100

    6.13 Created a channel for streaming new product comments to subscribers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://healingsound.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://healingsound.herokuapp.com', 'http://healingsound.herokuapp.com']
 end
